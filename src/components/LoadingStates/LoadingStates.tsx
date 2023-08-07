30e3c76df651f6fa855483c316dcce0772c1122f
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2023-08-07 10:45:44 -0400

    style loading states

--- src/components/LoadingStates/LoadingStates.tsx
+++ src/components/LoadingStates/LoadingStates.tsx
@@ -3,20 +3,35 @@ import React from 'react';
 import { MovieType } from '../../types/MovieType';
 
 export function LoadingState() {
-  return <p>Loading...</p>;
+  return (
+    <div className='flex h-full items-center justify-center'>
+      <p className='animate-pulse text-xl font-semibold text-blue-500'>
+        Loading...
+      </p>
+    </div>
+  );
 }
 
 export function FailureState() {
-  return <p>Failed to load</p>;
+  return (
+    <div className='flex h-full items-center justify-center'>
+      <p className='text-xl font-semibold text-red-500'>Failed to load</p>
+    </div>
+  );
 }
 
 type SuccessStateProps = {
   moviesArray: MovieType[];
   children: React.ReactNode;
 };
+
 export function SuccessState({ moviesArray, children }: SuccessStateProps) {
   if (moviesArray.length === 0) {
-    return <p>No movies found</p>;
+    return (
+      <div className='flex h-full items-center justify-center'>
+        <p className='text-xl font-semibold text-gray-500'>No movies found</p>
+      </div>
+    );
   }
 
   return <>{children}</>;
